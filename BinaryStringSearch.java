408ba36bececa236d3243fdddccb0313febcca7b
ryorkmiguel <49031612+ryorkmiguel@users.noreply.github.com>
2019-04-15 11:20:10 -0400

    BinaryStringSearch.java
    Use this algorithm:
    
    Binary search
    pointer = 0
    left extreme = 0
    right extreme = arraylen - 1
    loop
    pointer = le + (re - le)/2 
    array[pointer].compareTo(search word)
    0: return, found match
    negative: le = pointer, recurse
    positive: re = pointer, recurse
    
    Use the random words from the hangman assignment
    
    read the random words using file io
    
    the file should be sorted but sort anyway. If you use insertion sort, you can sort as you read in a new word.
    
    Get user input for search word
    
    If the word is found, return index
    
    else return "Not found"

---
+++ BinaryStringSearch.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.srccodes.examples;
+
+/**
+ * @author yorkmiguel
+ *
+ */
+public class BinaryStringSearch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String[] arr = { "contribute", "geeks", "ide", "practice"}; 
+        String x = "ide"; 
+        int result = binarySearch(arr, x); 
+  
+        if (result == -1) 
+            System.out.println("Element not present"); 
+        else
+            System.out.println("Element found at "
+                              + "index " + result);
+
+	}
+	
+	// Returns index of x if it is present in arr[], 
+    // else return -1 
+    static int binarySearch(String[] arr, String x) 
+    { 
+        int l = 0, r = arr.length - 1; 
+        while (l <= r) { 
+            int m = l + (r - l) / 2; 
+  
+            int res = x.compareTo(arr[m]); 
+  
+            // Check if x is present at mid 
+            if (res == 0) 
+                return m; 
+  
+            // If x greater, ignore left half 
+            if (res > 0) 
+                l = m + 1; 
+  
+            // If x is smaller, ignore right half 
+            else
+                r = m - 1; 
+        } 
+  
+        return -1; 
+    } 
+
+}
